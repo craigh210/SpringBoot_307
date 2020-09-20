0d35c0ba1115332acaf592ddad01940327b0573a
craigh210 <craighunt210@gmail.com>
2020-09-19 22:40:55 -0400

    Second commit

---
+++ src/test/java/com/example/jwd307/SpringBoot307ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwd307;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot307ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
